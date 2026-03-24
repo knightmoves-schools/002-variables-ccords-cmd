e441d2ba9e687114a27e9b6be850f0da7ef704ae
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-24 12:16:44 -0500

    Fix class definition and constants in Course.cs

--- Course.cs
+++ Course.cs
@@ -1,8 +1,8 @@
 ﻿namespace knightmoves;
 public class Course
 {   
-public class int MaxStudents = 25;
-public const class int PerfectScore = 100;  
+public class MaxStudents = 25;
+public const class PerfectScore = 100;  
     public int getCourseLength() {
         int courseLength = 20;
         return courseLength;
