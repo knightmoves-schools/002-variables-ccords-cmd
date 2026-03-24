95cc19304f99bbe323b55d4e68c945042148e374
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-24 12:04:07 -0500

    Refactor Course class structure and formatting

--- Course.cs
+++ Course.cs
@@ -2,7 +2,7 @@
 public class Course
 {   
 public class MaxStudents = 25;
-public const class PerfectScore = 100;  
+public class PerfectScore = 100;  
     public int getCourseLength() {
         int courseLength = 20;
         return courseLength;
