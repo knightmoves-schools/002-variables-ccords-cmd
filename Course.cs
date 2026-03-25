950af52766fed21343904028d13d015b0f478093
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-25 10:16:08 -0500

    Update Course.cs

--- Course.cs
+++ Course.cs
@@ -2,7 +2,7 @@
 public class Course
 {   
 public class MaxStudents = 25;
-public const class PerfectScore = 100;  
+public const int PerfectScore = 100;  
     public int getCourseLength() {
         int courseLength = 20;
         return courseLength;
