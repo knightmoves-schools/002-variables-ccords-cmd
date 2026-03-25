e22ce3436b408e55492505fc61211bb2835a5615
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-25 10:24:24 -0500

    Fix variable declaration for courseLength in Course class

--- Course.cs
+++ Course.cs
@@ -4,6 +4,7 @@ public class Course
 public int MaxStudents = 25;
 public const int PerfectScore = 100;  
     public int CourseLength() {
+        int courseLength;
         courseLength = 20;
         return courseLength;
     }
