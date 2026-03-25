2b3ee80e04698ddc66a03b3d08095972f72a8fd0
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-25 10:17:25 -0500

    Fix class declaration and structure in Course.cs

--- Course.cs
+++ Course.cs
@@ -2,6 +2,7 @@
 public class Course
 {   
 public class MaxStudents = 25;
+public class PerfectScore;
 public const int PerfectScore = 100;  
     public int getCourseLength() {
         int courseLength = 20;
